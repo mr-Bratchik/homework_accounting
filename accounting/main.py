e6bdc3099118dc3ffc27fe56c58442cf872d5a75
Aleksander Bratchik <33engineer33@gmail.com>
2024-11-25 23:50:54 +0300

    Update

--- accounting/main.py
+++ accounting/main.py
@@ -1,8 +1,11 @@
 from application.salary import calculate_salary
 from application.db.people import get_employees
 from datetime import datetime
+from tqdm import tqdm
 
 if __name__ == '__main__':
     print(f"Текущая дата: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    for i in tqdm(range(10000000)):
+        pass
     calculate_salary()
     get_employees()
